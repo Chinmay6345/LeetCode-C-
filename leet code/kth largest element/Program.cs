bcc7cc0c40a32e29a9066fe995ead1e0dbf428bb
Chinmay6345 <chinmay8116@gmail.com>
2019-12-11 22:55:30 +0530

    intial commit

---
+++ leet code/kth largest element/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kth_largest_element
+{
+    //https://leetcode.com/problems/kth-largest-element-in-an-array/
+    public static class AppHelper
+    {
+        public static int FindKthLargest(int[] array, int k)
+        {
+            Array.Sort<int>(array, new Comparison<int>((i1, i2) => i2.CompareTo(i1)));
+            int element = array[k - 1];
+            return element;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(new String[] { "," }, StringSplitOptions.RemoveEmptyEntries).Select(a => int.Parse(a)).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(AppHelper.FindKthLargest(array, n));
+            Console.ReadLine();
+        }
+    }
+}
