a82597631778b5d4f385306a307e1c308995714f
Chinmay6345 <chinmay8116@gmail.com>
2019-12-13 09:26:35 +0530

    made changes added static method.

--- leet code/Majority Element/Program.cs
+++ leet code/Majority Element/Program.cs
@@ -7,13 +7,22 @@ using System.Threading.Tasks;
 namespace Majority_Element
 {
     //https://leetcode.com/problems/majority-element/
+    public static class AppHelper
+    {
+        public static int MajorityElement(int [] array)
+        {
+            var p = array.GroupBy(a => a).Select(b => new { Key = b.Key, Count = b.Count() });
+            return p.OrderByDescending(a2 => a2.Count).Select(a1 => a1.Key).FirstOrDefault();
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
             int[] array =Console.ReadLine().Split(new String[] { "," }, StringSplitOptions.RemoveEmptyEntries).Select(a => int.Parse(a)).ToArray();
-            var p = array.GroupBy(a => a).Select(b => new { Key = b.Key, Count = b.Count() });
-            int x = p.OrderByDescending(a2 => a2.Count).Select(a1 => a1.Key).FirstOrDefault();
+            Console.WriteLine(AppHelper.MajorityElement(array));
+            Console.ReadLine();
         }
     }
 }
