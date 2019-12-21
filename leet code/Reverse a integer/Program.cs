5b0bfebd2202a5b78a38efb8440c2acc27a59806
Chinmay6345 <chinmay8116@gmail.com>
2019-12-21 16:39:17 +0530

    added single num III

---
+++ leet code/Reverse a integer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_a_integer
+{
+    public static class AppHelper
+    {   
+        public static int Reverse(int num)
+        {
+            try
+            {
+                int reverse = 0;
+                bool isnegative = false;
+                int returnvalue;
+                if (num < 0)
+                {
+                    isnegative = true;
+                    num = Math.Abs(num);
+                }
+                while (num > 0)
+                {
+                    reverse = checked((reverse * 10) + num % 10);
+                    num = num / 10;
+                }
+                if (reverse > int.MaxValue)
+                {
+                    return 0;
+                }
+                returnvalue = (isnegative == false) ? reverse : -Math.Abs(reverse);
+                return returnvalue;
+            }
+            catch(Exception)
+            {
+                return 0;
+            }
+           
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p= AppHelper.Reverse(1534236469);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ leet code/Single Number III/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Number_III
+{
+    //https://leetcode.com/problems/single-number-iii/
+    public static class AppHelper
+    {
+        public static int[] SingleNum(int[] nums)
+        {
+            try
+            {
+                if (nums.Length == 1)
+                {
+                    return nums;
+                }
+                else
+                {
+                    var result = nums.GroupBy(element => element).Where(a => a.Count() == 1);
+                    int[] resultarray = result.SelectMany(a => a).ToArray();
+                    return resultarray;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = AppHelper.SingleNum(new int[] { 1, 2, 1, 3, 2, 5 });
+            foreach(int i in nums)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
