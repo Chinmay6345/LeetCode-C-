306f56a5a42429bd24b089a6c6d411275df1623e
Chinmay6345 <chinmay8116@gmail.com>
2019-12-24 09:02:08 +0530

    commit-even numbers in digits in number

---
+++ leet code/Numbers with Even Number of Digits/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_with_Even_Number_of_Digits
+{
+    //https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
+    public static class AppHelper
+    {
+        public static Int32 GetCount(int [] nums)
+        {
+            int count = 0;
+            foreach(Int32 number in nums)
+            {
+                int NoOfDigits = (int)Math.Floor(Math.Log10(Math.Abs(number)) + 1);
+                if (NoOfDigits % 2 == 0)
+                    count++;
+            }
+            return count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AppHelper.GetCount(new int[] { 555, 901, 482, 1771 }));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ leet code/Numbers with Even Number of Digits/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Numbers with Even Number of Digits")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Numbers with Even Number of Digits")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4c4d0cea-5cb5-4059-a27a-82a00efa6df8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
