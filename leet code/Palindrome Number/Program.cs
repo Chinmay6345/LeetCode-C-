68ae26d0f7b4314fc7cbe52a77432ba570dff9cd
Chinmay6345 <chinmay8116@gmail.com>
2019-12-25 20:17:55 +0530

    Commit for palindrome integer

---
+++ leet code/Palindrome Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Palindrome_Number
+{
+    //https://leetcode.com/problems/palindrome-number/
+
+    public static class AppHelper
+    {
+       public static Boolean IsPalindrome(int number)
+       {
+            Int32 reverse = 0;
+            Int32 originalnumber = number;
+            Boolean isValid = false;
+            if (number < 0)
+                return false;
+            else
+            {
+                while(number >0)
+                {
+                    reverse = (reverse *10) + number % 10;
+                    number = number / 10;
+
+                }
+                if (reverse == originalnumber)
+                    isValid = true;
+            }
+            return isValid;
+       }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(AppHelper.IsPalindrome(121));
+            Console.ReadLine();
+        }
+    }
+}
