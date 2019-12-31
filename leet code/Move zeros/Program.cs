d5a4931592560420ed2de69820b8af5c15502ab9
Chinmay6345 <chinmay8116@gmail.com>
2020-01-01 00:03:42 +0530

    commit for third maximum and move zeros

---
+++ leet code/Move zeros/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Move_zeros
+{
+    //https://leetcode.com/problems/move-zeroes/
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array =  { 0, 0};        
+            Int32 zeros = Array.FindAll(array, e => e == 0).Count();
+            List<Int32> lst = array.Where(element => element != 0).ToList();
+            
+            for (int j = 0; j < zeros; j++)
+            {
+                lst.Add(0);
+            }
+            for(int k=0;k<lst.Count;k++)
+            {
+                array[k] = lst[k];
+            }
+            Console.ReadLine();
+        } 
+    }
+}
---
+++ leet code/Third Maximum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Third_Maximum
+{
+    public static class AppHelper
+    {
+        public static Int32 ThirdMaximum(Int32 [] nums,int k)
+        {
+            Int32 Length = nums.Length;
+            if(Length <3)
+            {
+                return nums[0];
+            }
+            nums= nums.OrderByDescending(element => element).ToArray<Int32>();
+            return nums[k - 1];
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = { 2, 2, 3, 1 };
+           int p = AppHelper.ThirdMaximum(array, 3);
+           Console.ReadLine();
+        }
+    }
+}
