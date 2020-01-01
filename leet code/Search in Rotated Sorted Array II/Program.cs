194717eae173173c93d5e8b465e98013e2074072
Chinmay6345 <chinmay8116@gmail.com>
2020-01-01 17:39:46 +0530

    commit for search in rotated array ||

---
+++ leet code/Search in Rotated Sorted Array II/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_in_Rotated_Sorted_Array_II
+{
+    //https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+    public static class AppHelper
+    {
+        public static Boolean Search(int[] nums, int target)
+        {
+            if (nums.Length == 0)
+            {
+                return false;
+            }
+            if (nums.Any(a=>a== target))
+            {
+                return true;
+            }
+            else
+                return false;            
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = { 1 };
+            bool p = AppHelper.Search(array, 1);
+            Console.ReadLine();
+        }
+    }
+}
