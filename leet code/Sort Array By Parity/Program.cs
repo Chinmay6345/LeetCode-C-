8d0b62b59007023ebd382ccb2f500e17a7b36b2e
chinmay6345 <chinmay8116@gmail.com>
2020-01-17 08:30:42 +0530

    commit

---
+++ leet code/Sort Array By Parity/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+//https://leetcode.com/problems/sort-array-by-parity/
+namespace Sort_Array_By_Parity
+{
+    public static class AppHelper
+    {
+        public static Int32[] Sorted(Int32[] array)
+        {
+            return array.OrderBy(a=>a %2 ==0 ? -a : a).ToArray();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32[] array = AppHelper.Sorted(new int[] { 4, 1, 3, 2 });
+            Console.Write(String.Join(" ", array.Select(a => a)));
+        }
+    }
+}
