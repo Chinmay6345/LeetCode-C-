05511ff1704448933592ed9289fdd21ba5b0ab3e
chinmay6345 <chinmay8116@gmail.com>
2020-01-22 06:57:54 +0530

    commit

---
+++ leet code/Integer to the Sum of Two No-Zero Integers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Integer_to_the_Sum_of_Two_No_Zero_Integers
+{
+    public static class Solution
+    {
+        public static int[] GetNoZeroIntegers(int n)
+        {
+            Int32[] array = new Int32[2];
+            int k = n;
+            for (int i = 1; i < n; i++)
+            {
+                k = k - 1;
+                if ((!i.ToString().Contains("0") && !k.ToString().Contains("0")))
+                {
+                    if (i + k == n)
+                    {
+                        array[0] = i;
+                        array[1] = k;
+                        break;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Int32[] array = Solution.GetNoZeroIntegers(4);
+        }
+    }
+}
