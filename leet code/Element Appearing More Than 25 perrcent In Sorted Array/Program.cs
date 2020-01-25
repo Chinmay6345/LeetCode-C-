a4c4c548c932a84e0a6e6627246713dcf176792c
chinmay6345 <chinmay8116@gmail.com>
2020-01-25 10:24:29 +0530

    commit

---
+++ leet code/Element Appearing More Than 25 perrcent In Sorted Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+//https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+
+namespace Element_Appearing_More_Than_25_perrcent_In_Sorted_Array
+{
+    public class Solution
+    {
+        public int FindSpecialInteger(int[] arr)
+        {
+            return arr.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            Int32 k = sol.FindSpecialInteger(new int[] { 1, 2, 2, 6, 6, 6, 6, 7, 10 });
+            Console.WriteLine(k);
+            Console.ReadLine();
+        }
+    }
+}
