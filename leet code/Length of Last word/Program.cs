a9572edabc8c10180b183b2283be30f307b004c4
chinmay6345 <chinmay8116@gmail.com>
2020-01-25 17:37:57 +0530

    commit

---
+++ leet code/Length of Last word/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//https://leetcode.com/problems/length-of-last-word/
+namespace Length_of_Last_word
+{
+    public static class AppHelper
+    {
+        public static int lengthOfLastWord(string a)
+        {
+            bool char_flag = false;
+            int len = 0;
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                if (char.IsLetter(a[i]))
+                {
+                    char_flag = true;
+                    len++;
+                }
+                else
+                {
+                    if (char_flag == true)
+                    {
+                        return len;
+                    }
+                }
+            }
+            return len;
+        }
+    }
+
+
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            return AppHelper.lengthOfLastWord(s);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution s = new Solution();
+            int k = s.LengthOfLastWord("hello world");
+        }
+    }
+}
