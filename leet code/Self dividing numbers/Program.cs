7168f97e04629415208f609642a496567b99b800
chinmay6345 <chinmay8116@gmail.com>
2020-01-25 17:26:59 +0530

    commit

---
+++ leet code/Self dividing numbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/self-dividing-numbers/submissions/
+
+namespace Self_dividing_numbers
+{
+    public static class Utility
+    {
+        public static bool isSelfDividing(int n)
+        {
+            int currentDigit = 0;
+            int actualNumber = n;
+            while (n > 0)
+            {
+                currentDigit = n % 10;
+                if ((currentDigit == 0) || (actualNumber % currentDigit != 0))
+                {
+                    return false;
+                }
+                n /= 10;
+            }
+            return true;
+        }
+
+        public static List<Int32> SelfDividingNum(Int32 low, Int32 high)
+        {
+            List<Int32> LstSelfDividingNum = new List<Int32>();
+            for (int i = low; i <= high; i++)
+            {
+                if (isSelfDividing(i))
+                {
+                    LstSelfDividingNum.Add(i);
+                }
+            }
+            return LstSelfDividingNum;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(Int32 num in Utility.SelfDividingNum(1,22))
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ leet code/Set Mismatchh/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//https://leetcode.com/problems/set-mismatch/
+
+namespace Set_Mismatchh
+{
+    public static class AppHelper
+    {
+        public static int[] MisMatch(int[] nums)
+        {
+            Array.Sort(nums);
+            int dup = -1, missing = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int k = nums[i];
+                int k1 = nums[i - 1];
+                if (k == k1)
+                    dup = k;
+                else if (k > k1 + 1)
+                    missing = k1 + 1;
+            }
+            int x = nums.Length;
+            int y = nums[x - 1];
+            return new int[] { dup, x != y ? x : missing };
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32[] array = AppHelper.MisMatch(new int[] { 1, 2, 2, 4 });
+            Console.ReadLine();
+        }
+    }
+}
