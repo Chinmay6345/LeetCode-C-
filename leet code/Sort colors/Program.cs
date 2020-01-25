82e96b8b7a6c329efda4b0d82329e1e2b2d21bda
chinmay6345 <chinmay8116@gmail.com>
2020-01-25 17:49:20 +0530

    commit

---
+++ leet code/Sort colors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_colors
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+            Array.Sort<int>(nums, new Comparison<int>(
+                       (i1, i2) => i1.CompareTo(i2)));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.SortColors(new int[] {2,0,2,1,1,0 });
+        }
+    }
+}
