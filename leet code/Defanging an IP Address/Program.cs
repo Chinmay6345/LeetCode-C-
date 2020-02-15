c6b8e402870e7069ef1a5b67d9172d0c5138671f
chinmay6345 <chinmay8116@gmail.com>
2020-02-15 18:14:34 +0530

    commit

---
+++ leet code/Defanging an IP Address/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/defanging-an-ip-address/
+namespace Defanging_an_IP_Address
+{
+    public static class Solution
+    {
+        public static string DefangIPaddr(string address)
+        {
+            String defangIPAddress = String.Empty;
+            defangIPAddress = address.Replace(".", "[.]");
+            return defangIPAddress;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Solution.DefangIPaddr("1.1.1.1");
+        }
+    }
+}
