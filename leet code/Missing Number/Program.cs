304bbc7ab147200a06660cdc0b456b81b1183979
chinmay6345 <chinmay8116@gmail.com>
2020-02-15 18:35:23 +0530

    commit

---
+++ leet code/Missing Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/missing-number/
+
+namespace Missing_Number
+{
+    public static class AppHelper
+    {
+        public static Int32 Missing(int[] array)
+        {
+            int missing = 0;
+            Array.Sort<int>(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == i)
+                {
+                    missing = missing + 1;
+                }
+            }
+            return missing;
+        }
+    }
+
+
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            return AppHelper.Missing(nums);
+        }
+    }
+}
