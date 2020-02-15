6bbe49867d3ed936f65baf818f5b3c8104cefab7
chinmay6345 <chinmay8116@gmail.com>
2020-02-15 18:48:49 +0530

    commit

---
+++ leet code/Excel Sheet Column Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//https://leetcode.com/problems/excel-sheet-column-number/
+namespace Excel_Sheet_Column_Number
+{
+    public static class AppHelper
+    {
+        public static Int32 ColumnLetterToColumnIndex(string columnLetter)
+        {
+            columnLetter = columnLetter.ToUpper();
+            int sum = 0;
+
+            for (int i = 0; i < columnLetter.Length; i++)
+            {
+                sum *= 26;
+                sum += (columnLetter[i] - 'A' + 1);
+            }
+            return sum;
+        }
+    }
+
+
+    public class Solution
+    {
+        public Int32 TitleToNumber(string s)
+        {
+            return AppHelper.ColumnLetterToColumnIndex(s);
+        }
+    }
+}
---
+++ leet code/Excel Sheet Column Title/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/excel-sheet-column-title/
+
+namespace Excel_Sheet_Column_Title
+{
+    public static class AppHelper
+    {
+        public static string GetExcelColumnName(int columnNumber)
+        {
+            int dividend = columnNumber;
+            string columnName = String.Empty;
+            int modulo;
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            return columnName;
+        }
+    }
+
+    public class Solution
+    {
+        public string ConvertToTitle(int n)
+        {
+            return AppHelper.GetExcelColumnName(n);
+        }
+    }
+}
