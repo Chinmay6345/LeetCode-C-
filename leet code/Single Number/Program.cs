705c65fc10ff0709a22290d6e7390db825a00f87
chinmay6345 <chinmay8116@gmail.com>
2020-02-15 22:19:53 +0530

    commit

---
+++ leet code/Single Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+//https://leetcode.com/problems/single-number/
+namespace Single_Number
+{
+    public static class AppHelper
+    {
+        public static int SingleNum(int[] nums)
+        {
+            try
+            {
+                if (nums.Length == 1)
+                {
+                    return nums[0];
+                }
+                else
+                {
+                    var result = nums.GroupBy(element => element).Where(a => a.Count() == 1).FirstOrDefault();
+                    return Convert.ToInt32(result.Key.ToString());
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            return AppHelper.SingleNum(nums);
+        }
+    }
+}
