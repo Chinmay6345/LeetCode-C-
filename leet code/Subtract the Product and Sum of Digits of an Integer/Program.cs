1f2ea7034f2455e4125b2890b6bc61491237930c
chinmay6345 <chinmay8116@gmail.com>
2020-02-15 19:36:26 +0530

    commit

---
+++ leet code/Subtract the Product and Sum of Digits of an Integer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+namespace Subtract_the_Product_and_Sum_of_Digits_of_an_Integer
+{
+    public class Solution
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            Int32 multiply = 1;
+            Int32 sum = 0;
+            foreach (Char item in n.ToString())
+            {
+                multiply = multiply * (int)Char.GetNumericValue(item);
+                sum = sum + (int)Char.GetNumericValue(item);
+            }
+            Int32 difference = multiply - sum;
+            return difference;
+        }
+    }
+}
