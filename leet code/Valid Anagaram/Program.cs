2fae90fd914e2f9f37b8ac2aadf17286298ff580
chinmay6345 <chinmay8116@gmail.com>
2020-02-15 19:07:15 +0530

    commit

---
+++ leet code/Valid Anagaram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/valid-anagram/submissions/
+namespace Valid_Anagaram
+{
+    public static class Solution
+    {
+        public static bool IsAnagram(string a, string b)
+        {
+            return String.Concat(a.OrderBy(c => c)).Equals(String.Concat(b.OrderBy(c => c)));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isvalid = Solution.IsAnagram("anagram", "nagaram");
+        }
+    }
+}
