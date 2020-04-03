e8312ad96d322a73e0a317896d2db984ab5323dd
chinmay6345 <chinmay8116@gmail.com>
2020-04-03 16:44:53 +0530

    added code using two pointers

--- leet code/Sort Array By Parity/Program.cs
+++ leet code/Sort Array By Parity/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 //https://leetcode.com/problems/sort-array-by-parity/
@@ -6,10 +7,38 @@ namespace Sort_Array_By_Parity
 {
     public static class AppHelper
     {
+        //using linq
         public static Int32[] Sorted(Int32[] array)
         {
-            return array.OrderBy(a=>a %2 ==0 ? -a : a).ToArray();
+            return array.OrderBy(a => a % 2 == 0 ? -a : a).ToArray();
         }
+        //using two pointer
+        public static Int32[] Sort(Int32[] arr)
+        {
+            Int32 temp;
+            if (arr.Length == 1 || arr.Length == 0)
+            {
+                return arr;
+            }
+            Int32 i = 0;
+            Int32 j = arr.Length - 1;
+            while (i < j)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return arr;
+        }
+        
     }
 
     class Program
