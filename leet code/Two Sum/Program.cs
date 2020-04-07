14a9c7da39c02dadd5d7d4889662098766ca73df
chinmay6345 <chinmay8116@gmail.com>
2020-04-07 10:13:55 +0530

    commit

--- leet code/Two Sum/Program.cs
+++ leet code/Two Sum/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 //https://leetcode.com/problems/two-sum/
 /*
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -13,6 +14,7 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1]. 
 */
+
 namespace Two_Sum
 {
     public static class AppHelper
