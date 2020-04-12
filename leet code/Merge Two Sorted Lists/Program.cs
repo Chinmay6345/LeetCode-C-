233001d29531230abcc188b41a5bbdde4916ad80
chinmay6345 <chinmay8116@gmail.com>
2020-04-12 11:53:52 +0530

    added link and descripton-merge two sorted lists

--- leet code/Merge Two Sorted Lists/Program.cs
+++ leet code/Merge Two Sorted Lists/Program.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//https://leetcode.com/problems/merge-two-sorted-lists/
+/*
+ Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
 namespace Merge_Two_Sorted_Lists
 {
 
