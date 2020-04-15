8a2ccb65fce064dfb8a14ea539b6010bab482bde
Chinmay <Chinmay8116@gmail.com>
2020-04-15 20:39:54 +0530

    commit-added description

--- leet code/N-Repeated Element in Size 2N Array/Program.cs
+++ leet code/N-Repeated Element in Size 2N Array/Program.cs
@@ -1,7 +1,26 @@
 ﻿using System.Linq;
 using System;
 //https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+/*
+ In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
 
+Return the element repeated N times.
+
+ 
+
+Example 1:
+
+Input: [1,2,3,3]
+Output: 3
+Example 2:
+
+Input: [2,1,2,5,3,2]
+Output: 2
+Example 3:
+
+Input: [5,1,5,2,5,3,5,4]
+Output: 5
+ */
 namespace N_Repeated_Element_in_Size_2N_Array
 {
     public static class AppHelper
