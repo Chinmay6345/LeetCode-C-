c4ca8d4a5d840ffd86b3b4bd5021191c712534d1
Chinmay <Chinmay8116@gmail.com>
2020-04-16 16:32:37 +0530

    commit-remove LL elements

---
+++ leet code/Remove Linked List elements/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+//https://leetcode.com/problems/remove-linked-list-elements/
+/*
+ Remove all elements from a linked list of integers that have value val.
+
+Example:
+
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+ */
+namespace Remove_Linked_List_elements
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+    public class AppHelper
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+        public ListNode Remove(ListNode head, Int32 val)
+        {
+            ListNode p = head;
+            ListNode q = null;
+            if (head == null)
+            {
+                return head;
+            }
+            while (p != null && p.val == val)
+            {
+                head = p.next;
+                p = head;
+            }
+            while (p != null)
+            {
+                while (p != null && p.val != val)
+                {
+                    q = p;
+                    p = p.next;
+                }
+                if (p == null)
+                {
+                    return head;
+                }
+                q.next = p.next;
+                p = q.next;
+            }
+            return head;
+        }
+    }
+    public class Solution
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            AppHelper helper = new AppHelper();
+            return helper.Remove(head, val);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
