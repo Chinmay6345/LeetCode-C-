a3690349c802ee28b75e4376ec8a83cdc08137d6
Chinmay <Chinmay8116@gmail.com>
2020-07-22 17:07:27 +0530

    added Hset for missing number

--- leet code/Missing Number/Program.cs
+++ leet code/Missing Number/Program.cs
@@ -10,6 +10,24 @@ namespace Missing_Number
 {
     public static class AppHelper
     {
+        public static Int32 UsingHSet(Int32 [] arr)
+        {
+            ISet<Int32> HSet = new HashSet<Int32>();
+            for(Int32 i=0;i<arr.Length;i++)
+            {
+                HSet.Add(arr[i]);
+            }
+            Int32 totalNoOfElements = arr.Length + 1;
+            for(Int32 i=0;i<totalNoOfElements;i++)
+            {
+                if(!HSet.Contains(i))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
         public static Int32 Missing(int[] array)
         {
             int missing = 0;
