bab368ae61e4fa0194ef76f8534c15aab198d2fa
Chinmay <Chinmay8116@gmail.com>
2020-07-22 15:52:50 +0530

    commit power of two

---
+++ leet code/Power of two/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/power-of-two/submissions/
+namespace Power_of_two
+{
+    public static class AppHelper
+    {
+        public static Boolean PowerofTwo(Int32 n)
+        {
+            if (n == 0)
+            {
+                return false;
+            }
+            while (n != 1)
+            {
+                if (n % 2 != 0)
+                {
+                    return false;
+                }
+                n = n / 2;
+            }
+            return true;
+        }
+    }
+    public class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            return AppHelper.PowerofTwo(n);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
